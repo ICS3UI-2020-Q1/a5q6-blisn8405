91bb4971a3077f6bc37345d3b7a911fa597c7a5f
blisn8405 <blisn8405@wrdsb.ca>
2020-10-06 17:04:49 +0000

    q6

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 /**
  *
  * @author 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+      // create a scanner for user input
+Scanner input = new Scanner(System.in);
+    //get users number
+System.out.println("Please enter a number between 1 and 10");
+   int number = input.nextInt(); 
+   for(int i = number; i >= 1; i--){
+     System.out.println();
+     for(int j = number; j >= 1; j--){
+       System.out.print("*");
+
+     }
+  }
   }
 }
